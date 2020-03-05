520cfa0c42f1b754c33facdef46b4d9c6c63fcfe
Andy Alm <andy.alm@gettyimages.com>
2020-03-04 16:11:00 -0800

    WIP

--- FaissSharp/Internal/IndexIDMapSafeHandle.cs
+++ FaissSharp/Internal/IndexIDMapSafeHandle.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FaissSharp.Internal
 {
     internal class IndexIDMapSafeHandle : IndexSafeHandle
@@ -9,6 +11,9 @@ namespace FaissSharp.Internal
             _native.faiss_IndexIDMap_new(ref idIndex, index);
             return idIndex;
         }
+        
+        public IndexIDMapSafeHandle() {}
+        public IndexIDMapSafeHandle(IntPtr pointer) : base(pointer) {}
         public void Add(long count, float[] vectors, long[] ids)
         {
             _native.faiss_Index_add_with_ids(this, count, vectors, ids);
