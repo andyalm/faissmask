e7b9ab38489c0486ce8cf5c942bce32133562cc8
Andy Alm <andy.alm@gettyimages.com>
2022-03-23 10:29:51 -0700

    Added support for building on mac arm64

--- build-faiss-macos.sh
+++ build-faiss-macos.sh
@@ -8,6 +8,6 @@ git clone --recursive --branch $BRANCH https://github.com/facebookresearch/faiss
 cd libfaiss-src
 cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=ON -B build .
 make -C build -j faiss faiss_avx2
-make -C build install
+sudo make -C build install
 cp build/c_api/libfaiss_c.dylib ../FaissMask/runtimes/osx-x64/native/
 cd ..
