b42985c1daa972567585d1a0f9558eb808de638f
Andy Alm <andy.alm@gettyimages.com>
2022-07-06 16:47:23 -0700

    Updated the osx-arm64 binary with changes from latest Dockerfile

--- build-faiss-macos.sh
+++ build-faiss-macos.sh
@@ -9,5 +9,11 @@ cd libfaiss-src
 cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=ON -B build .
 make -C build -j faiss
 sudo make -C build install
-cp build/c_api/libfaiss_c.dylib ../FaissMask/runtimes/osx-x64/native/
+
+arch=arm64
+if [[ $(uname -m) == 'x86_64' ]]; then
+  arch=x64
+fi
+
+cp build/c_api/libfaiss_c.dylib ../FaissMask/runtimes/osx-$arch/native/
 cd ..
